6dd3be3d758dc19440ef81f955e3ae3cd124d4f9
Danilo Jr Arizala <danilo.arizala@gmail.com>
2024-04-25 08:51:16 +0800

    Add blog.njk file

---
+++ .eleventy.js
@@ -0,0 +1,37 @@
+const { DateTime } = require("luxon");
+const markdownFilter = require("./markdown-filter");
+const sitemap = require("@quasibit/eleventy-plugin-sitemap");
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPlugin(sitemap, {
+        sitemap: {
+            hostname: "https://www.omnixlabs.com/"
+        },
+    });
+
+    eleventyConfig.addCollection("sitemap", function(collectionApi) {
+        const excludedDirs = ['admin', 'netlify', 'authors', 'static'];
+        return collectionApi.getAll().filter(item => {
+          // Exclude items whose URL path includes any of the excluded directories
+          return !excludedDirs.some(dir => item.url.includes('/' + dir + '/'));
+        });
+      });
+      
+    eleventyConfig.addPassthroughCopy('./src/img');
+    eleventyConfig.addPassthroughCopy('./src/admin');
+    eleventyConfig.addPassthroughCopy('./src/css');
+    eleventyConfig.addPassthroughCopy('./src/js');
+    eleventyConfig.addPassthroughCopy('./src/about/*.html');
+
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+    });
+    eleventyConfig.addFilter("markdownFilter", markdownFilter);
+
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    };
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+These are the starting files used by [OMNIX](https://www.omnixlabs.com/) to create the main consumer facing website. The website is built using JAMstack. 
+
+The provided files are finished pages using regular HTML and CSS. The HTML files can be broken down into pieces, and used to build out the different templates and partial files using Nunjucks and 11ty.
